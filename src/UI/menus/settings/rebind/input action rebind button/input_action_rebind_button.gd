8fb11fad4e0a3666beaa9851a98e13ec59181d8e
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-06 13:59:39 +0300

    allow rebinding keys

--- src/UI/menus/settings/rebind/input action rebind button/input_action_rebind_button.gd
+++ src/UI/menus/settings/rebind/input action rebind button/input_action_rebind_button.gd
@@ -2,11 +2,17 @@ class_name InputActionRebindButton
 extends Button
 
 
+const REMAPING_TEXT: String = 'Press a key to select'
+
+signal remaping(input_action_rebind_button: InputActionRebindButton)
+
 @export var action_name: String : set = _set_action_name
-@export var action_label: String
+@export var action_text: String
 
 @onready var action_name_label: Label = $MarginContainer/HBoxContainer/ActionNameLabel
 @onready var action_input_label: Label = $MarginContainer/HBoxContainer/ActionInputLabel
+
+var is_remaping: bool = false : set = _set_is_remapping
 
 
 func _ready() -> void:
@@ -17,9 +23,25 @@ func _set_action_name(new_action_name: String) -> void:
 	action_name = new_action_name
 	
 	if action_name_label:
-		action_name_label.text = action_label
+		action_name_label.text = action_text
 	
+	_set_action_input_label()
+
+
+func _set_action_input_label():
 	if action_input_label:
 		var events = InputMap.action_get_events(action_name)
-		print(action_name)
 		action_input_label.text = events[0].as_text().trim_suffix(' (Physical)') if events.size() > 0 else ""
+
+
+func _on_pressed() -> void:
+	is_remaping = true
+
+
+func _set_is_remapping(new_is_remapping: bool):
+	is_remaping = new_is_remapping
+	if is_remaping:
+		action_input_label.text = REMAPING_TEXT
+		remaping.emit(self)
+	else:
+		_set_action_input_label()
--- src/UI/menus/settings/rebind/rebind_menu.gd
+++ src/UI/menus/settings/rebind/rebind_menu.gd
@@ -6,9 +6,23 @@ const INPUT_ACTION_REBIND_BUTTON = preload("res://src/UI/menus/settings/rebind/i
 
 @onready var actions_list: VBoxContainer = $Panel/MarginContainer/VBoxContainer/ScrollContainer/ActionsList
 
+var remapping_button: InputActionRebindButton = null
+
 
 func _ready() -> void:
 	_create_actions_list()
+
+
+func _input(event: InputEvent) -> void:
+	if remapping_button:
+		if (
+			event is InputEventKey ||
+			(event is InputEventMouseButton && event.is_pressed())
+		):
+			InputMap.action_erase_events(remapping_button.action_name)
+			InputMap.action_add_event(remapping_button.action_name, event)
+			remapping_button.is_remaping =  false
+			remapping_button = null
 
 
 func _create_actions_list() -> void:
@@ -16,6 +30,11 @@ func _create_actions_list() -> void:
 		var new_input_action_button: InputActionRebindButton = INPUT_ACTION_REBIND_BUTTON.instantiate()
 		
 		new_input_action_button.action_name = action
-		new_input_action_button.action_label = InputActionsToRebind.input_actions[action]
+		new_input_action_button.action_text = InputActionsToRebind.input_actions[action]
+		new_input_action_button.remaping.connect(_on_action_pressed)
 		
 		actions_list.add_child(new_input_action_button)
+
+
+func _on_action_pressed(action: InputActionRebindButton):
+	remapping_button = action
