558b22d9339b02f1d10e76cd794ff04b92eddfd9
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-07 00:36:08 +0300

    init main menu

--- src/levels/sandbox_1.gd
+++ src/levels/sandbox_1.gd
@@ -6,3 +6,11 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	icon.rotation += delta * 2
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("example1"):
+		print('ex1')
+	if event.is_action_pressed("example2"):
+		print('ex2')
+	if event.is_action_pressed("example3"):
+		print('ex3')
