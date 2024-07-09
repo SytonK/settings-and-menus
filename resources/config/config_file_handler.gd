92af0e95fc802d6104c4d07ac68a91274c747cf4
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-10 01:44:35 +0300

    add audio settings

--- resources/config/config_file_handler.gd
+++ resources/config/config_file_handler.gd
@@ -9,8 +9,27 @@ var config_file: ConfigFile = ConfigFile.new()
 
 func _ready() -> void:
 	if !FileAccess.file_exists(SETTINGS_FILE_PATH):
-		config_file.set_value("audio", "master_volum", 0)
-		config_file.set_value("audio", "music_volum", 0)
-		config_file.set_value("audio", "SFX_volum", 0)
+		set_default_audio_settings()
 		
 		config_file.save(SETTINGS_FILE_PATH)
+	else:
+		config_file.load(SETTINGS_FILE_PATH)
+
+
+func set_default_audio_settings() -> void:
+	config_file.set_value("audio", "master_muted", false)
+	config_file.set_value("audio", "master_volum", 0)
+	config_file.set_value("audio", "music_muted", false)
+	config_file.set_value("audio", "music_volum", 0)
+	config_file.set_value("audio", "SFX_muted", false)
+	config_file.set_value("audio", "SFX_volum", 0)
+
+func save_audio_settings(key: String, value):
+	config_file.set_value('audio', key, value)
+	config_file.save(SETTINGS_FILE_PATH)
+
+func load_audio_settings() -> Dictionary:
+	var audio_settings = {}
+	for key in config_file.get_section_keys('audio'):
+		audio_settings[key] = config_file.get_value('audio', key)
+	return audio_settings
