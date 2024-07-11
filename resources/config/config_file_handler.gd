f590cc287c83db401bec74cfff1b6a14379434af
SytonK <syton98k@gmail.com>
2024-07-11 10:26:04 +0300

    privet default and load settings

--- resources/config/config_file_handler.gd
+++ resources/config/config_file_handler.gd
@@ -9,15 +9,16 @@ var config_file: ConfigFile = ConfigFile.new()
 
 func _ready() -> void:
 	if !FileAccess.file_exists(SETTINGS_FILE_PATH):
-		set_default_audio_settings()
+		_set_default_audio_settings()
 		
 		config_file.save(SETTINGS_FILE_PATH)
 	else:
 		config_file.load(SETTINGS_FILE_PATH)
-		load_audio_settings()
+		
+		_load_audio_settings()
 
 
-func set_default_audio_settings() -> void:
+func _set_default_audio_settings() -> void:
 	config_file.set_value("audio", "master_muted", false)
 	config_file.set_value("audio", "master_volum", 0)
 	config_file.set_value("audio", "music_muted", false)
@@ -29,7 +30,7 @@ func save_audio_settings(key: String, value):
 	config_file.set_value('audio', key, value)
 	config_file.save(SETTINGS_FILE_PATH)
 
-func load_audio_settings() -> void:
+func _load_audio_settings() -> void:
 	AudioServer.set_bus_mute(0, config_file.get_value('audio', 'master_muted'))
 	AudioServer.set_bus_volume_db(0, config_file.get_value('audio', 'master_volum'))
 	AudioServer.set_bus_mute(1, config_file.get_value('audio', 'music_muted'))
