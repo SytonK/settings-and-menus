3ffae1876b7a2c9854def0bb6e7a7188247673f8
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-14 01:31:44 +0300

    settings menu auto focus

--- src/UI/menus/settings/settings_menu.gd
+++ src/UI/menus/settings/settings_menu.gd
@@ -6,6 +6,12 @@ extends Control
 
 @onready var curr_settings_menu: Control = rebind_menu : set = _set_curr_settings_menu
 
+@onready var keybinds_settings_button: Button = $VBoxContainer/HBoxContainer/KeybindsSettingsButton
+
+
+func _ready() -> void:
+	keybinds_settings_button.grab_focus()
+
 
 func _on_keybinds_settings_button_pressed() -> void:
 	curr_settings_menu = rebind_menu
