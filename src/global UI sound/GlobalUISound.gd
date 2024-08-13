29add6b28f17fe0a9d43a4630b77da540b821d02
SytonK <syton98k@gmail.com>
2024-08-13 14:25:51 +0300

    GlobalSoundUI push all

---
+++ src/global UI sound/GlobalUISound.gd
@@ -0,0 +1,19 @@
+class_name GlobalUISound extends Node
+
+
+@onready var sounds = {
+	&"UIHover": AudioStreamPlayer.new(),
+	&"UIClick": AudioStreamPlayer.new()
+}
+
+
+func _ready() -> void:
+	for sound in sounds.keys():
+		sounds[sound].stream = load("res://assets/sound/effects/" + str(sound) + ".mp3")
+		sounds[sound].bus = &"UISound"
+		sounds[sound].name = str(sound) + "AudioStreamPlayer"
+		add_child(sounds[sound])
+
+
+func ui_sound_play(sound: String) -> void:
+	sounds[sound].play()
