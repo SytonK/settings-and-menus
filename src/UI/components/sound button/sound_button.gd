9288dcd6141856effaab56c4de44a5f7b4dc2df5
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-08-14 20:31:01 +0300

    move to init

--- src/UI/components/sound button/sound_button.gd
+++ src/UI/components/sound button/sound_button.gd
@@ -1,7 +1,7 @@
 class_name SoundButton extends Button
 
 
-func _ready() -> void:
+func _init() -> void:
 	mouse_entered.connect(GlobalUiSound.ui_sound_play.bind("UIHover"))
 	focus_entered.connect(GlobalUiSound.ui_sound_play.bind("UIHover"))
 	pressed.connect(GlobalUiSound.ui_sound_play.bind("UIClick"))
